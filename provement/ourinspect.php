2ecde289f979dd506855a9ae143bfc5c64545cc5
moeez70067298 <muhammadmoeez64@hmail.com>
2020-03-23 18:41:49 +0500

    provement
    our impact page work

---
+++ provement/ourinspect.php
@@ -0,0 +1,218 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>School Provement</title>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+              integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet">
+        <link rel="stylesheet" href="fontAwesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="owlcarousel/dist/assets/owl.carousel.min.css">
+        <link rel="stylesheet" href="owlcarousel/dist/assets/owl.theme.default.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    </head>
+    <body>
+            <header>
+        <div class="container-fluid col-md-12">
+            <nav class=" navbar navbar-expand-lg nav mx-5" style="background-color:#212529 ">
+                <a class="navbar-brand" href="#">
+                    <img src="images/logo1.png" style="height:60px;width: auto">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="fa fa-bars"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="  navbar-nav ">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="ourinspect.php">Our Imapact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">News</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><span class="fa fa-user"> Login</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+
+
+
+        </div>
+</header>
+
+    <center><h1 style="margin-bottom: 40px">OUR IMPACT</h1></center>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <iframe src="Week 4-HCI.pdf" width="100%" height="465px">
+                    </iframe>
+                </div>
+                <div class="col-md-4">
+                    <div class="col-md-9" style="background-color: #ccccff;height: 155px;width: 100%">
+                        <center><i class='fas fa-user-graduate' style='font-size:45px;margin-top: 20px'></i>
+                            <b><p style="font-size: 18px;color: #009900;margin-top: 10px">​​30 - 50% Less Absences</p></b>
+                            <h5 style="margin-top: -19px">for rural school teachers</h5>
+                            <p style="font-size: 10px;margin-top: -10px">(Absenteeism: 18.6% --> 10%)</p>
+                        </center>
+
+                    </div>
+                    <div class="col-md-9" style="background-color: #ccccff;height: 155px;width: 100%;margin-top: 10px" >
+                        <center><i class='far fa-smile' style='font-size:45px;margin-top: 20px'></i>
+                            <b><p style="font-size: 18px;color: #009900;margin-top: 10px">​​Higher Self-Efficacy</p></b>
+                            <p style="font-size: 10px;margin-top: -19px">Classrooms with a larger numbers of student and<br>
+                                parent users were shown to have stronger beliefs of<br>being able to positively impact school performance</p>
+                        </center>
+
+                    </div>
+                    <div class="col-md-9" style="background-color: #ccccff;height: 155px;width: 100%;margin-top: 10px" >
+                        <center><i class='fas fa-users' style='font-size:45px;margin-top: 20px'></i>
+                            <b><p style="font-size: 18px;color: #009900;margin-top: 10px">Broad Community Support</p></b>
+                            <p style="font-size: 10px;margin-top: -19px">A large majority of community members said that<br>
+                                the program should continue.<br>(7.1 out of 10 "Raise Your Text should continue")</p></center>
+                    </div>
+
+
+                </div>
+
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-11" style="background-color: #ccccff;width: 800px;height: 200px;margin-top: 30px;">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-3"  style="border-style: solid;border-width:1px;margin-top: 10px;height: 180px;width: 100%;border-color: black">
+                                    <center><p style="font-size: 35px">Pilot<br> Results</p>
+                                        <p>From 2018</p>
+                                    </center>
+                                </div>
+                                <div class="col-md-2" style="border-style: solid;border-width:1px;margin-top: 10px;height: 180px;width: 100%;border-color: black">
+                                    <center><p style="font-size: 35px;margin-top: 25px">16</p>
+                                        <p style="margin-top: 40px">Schools</p>
+                                    </center>
+
+                                </div>
+                                <div class="col-md-2" style="border-style: solid;border-width:1px;margin-top: 10px;height: 180px;width: 100%;border-color: black">
+                                    <center><p style="font-size: 35px;margin-top: 25px">128</p>
+                                        <p style="margin-top: 40px">Teachers</p>
+                                    </center>
+
+                                </div>
+                                <div class="col-md-2" style="border-style: solid;border-width:1px;margin-top: 10px;height: 180px;width: 100%;border-color: black">
+                                    <center><p style="font-size: 35px;margin-top: 25px">357</p>
+                                        <p style="margin-top: 40px">Non-Teacher Participants</p>
+                                    </center>
+
+                                </div>
+                                <div class="col-md-3" style="border-style: solid;border-width:1px;margin-top: 10px;height: 180px;width: 100%;border-color: black">
+                                    <center><p style="font-size: 35px;margin-top: 25px">220</p>
+                                        <p style="margin-top: 40px">Message Sent to the Program</p>
+                                    </center>
+
+                                </div>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-6" style="background-color: #ccccff;width:100%;height: 350px;margin-top: 30px">
+                        <b> <p style="margin-top: 25px;font-size: 25px">Our Future Impact Goals</p></b>
+                        <p>
+                            We want to holistically change how schools perform. This means better teacher attendance, habits, communication, and feedback.
+                        </p>
+                        <h4>By the end of 2020, we want to...</h4>
+                        <ul style="font-size: 20px">
+                            <li>Work in 100+ schools</li>
+                            <li>Reduce schools' absenteeism by 30+%</li>
+                            <li>Create positive habits for 10,000+ users</li>
+                            <li>Have 1000+ messages sent via our platform</li>
+
+                        </ul>
+
+                    </div>
+                    <div class="col-md-6" style="width:100%;height: 350px;margin-top: 30px">
+                        <img src="images/img1.jpg" alt="" style="width:445px;height: 350px" />
+                    </div>                
+
+                </div>
+            </div>
+
+
+        </div>
+        <br>
+        <br>
+    </section>
+    	<footer>
+		<div class="col-12">
+			<div class="row">
+				<div class="col-4 foot mt-3">
+					<h6>Site Map</h6>
+					<hr>
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Our Imapact</a></li>
+						<li><a href="#">News</a></li>
+						<li><a href="#">About Us</a></li>
+					</ul>
+				</div>
+				<div class="col-4 foot  mt-3">
+
+
+					<h6>Follow Us </h6>
+					<hr>
+					<p>Follow us on social media for all out latest news, updates and event information</p>
+					<a href="#"><img src="images/facebook.png" style="height: 30px"></a>
+				</div>
+				<div class="col-4 foot my-3">
+					<h6>Contact Us </h6>
+					<hr>
+					<form>
+						<div class="form-group">
+							<!-- <label>Full Name</label> -->
+							<input type="text" class="form-control" placeholder="Full Name" required>
+						</div>
+						<div class="form-group">
+							<!-- <label>Full Name</label> -->
+							<input type="number" class="form-control" placeholder="Phone" required>
+						</div>
+						<div class="form-group">
+							<!-- <label>Full Name</label> -->
+							<input type="email" class="form-control" placeholder="Email" required>
+						</div>
+						<input type="submit" value="Submit" class="btn btn-primary">
+					</form>
+
+				</div>
+
+			</div>
+
+
+
+		</div>
+
+	</footer>
+
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
