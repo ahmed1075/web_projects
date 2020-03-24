f2c5b16183b1042f13ce7166193cd1e54c89d8a9
moeez70067298 <muhammadmoeez64@hmail.com>
2020-03-24 22:08:33 +0500

    our inspect,blog and news

---
+++ provement/blog_news.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>School Provement</title>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+              integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet">
+        <link rel="stylesheet" href="fontAwesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="owlcarousel/dist/assets/owl.carousel.min.css">
+        <link rel="stylesheet" href="owlcarousel/dist/assets/owl.theme.default.min.css">
+    </head>
+
+    <body>
+        <header>
+            <div class="container-fluid">
+
+                <nav class=" navbar navbar-expand-lg nav mx-5">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="images/logo1.png" alt="" height="60px" width="auto">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="fa fa-bars"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul class="  navbar-nav ">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.html">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="ourinspect.php">Our Imapact</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="blog_news.php">News</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-fancyscroll="data-fancyscroll" href="#about">About Us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><span class="fa fa-user"> Login</span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <div id="demo" class="carousel slide my-carousol" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="images/img2.jpg" alt="Los Angeles" width="100%" height="300">
+                            <div class="carousel-caption">
+                                <hr style="background-color:white;width: 150px;height: 5px;">
+                                <h2 style="color:white">OUR LATEST NEWS & BLOG</h2>
+                                <hr style="background-color: white;width: 150px;height: 5px">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </header>
+
+        <main>
+            <div class="col-12 mt-4">
+
+                <div class="row">
+                    <center>
+                        <div class="col-md-8" style="margin-top: 80px">
+                            <img src="images/img4.PNG" width="600px" height="450px"/>
+                            <div class="col-md-8" style="margin-top: 30px">
+                                <p style="color: #666666">Friday, February 21, 2020 by Nicholas Canfield </p>
+                                <a href="#" style="text-decoration-line: none"><h3>Our Mission: 10 millions Students</h3></a>
+                                <p style="text-align: justify">Our mission: Get 10 million students better access to education through our services by 2025.
+                                    <br>
+                                    Support us at<br><a href="https://www.gofundme.com/f/improve-teacher-attendance-in-pohnpei-and-globally">https://www.gofundme.com/f/improve-teacher-attendance-in-pohnpei-and-globally </a></p>
+                            </div>
+                        </div>
+                        <a href="#" style="text-decoration: none;margin-left: 400px">400 Comments</a>
+                        <hr style="background-color:#0066cc;width: 800px;height: 3px">
+
+                        <div class="col-md-8" style="margin-top: 80px">
+                            <img src="images/img3.PNG" width="600px" height="450px"/>
+                            <div class="col-md-8" style="margin-top: 30px">
+                                <p style="color: #666666">Thursday, January 23, 2020 by Nicholas Canfield </p>
+                                <a href="#" style="text-decoration-line: none"><h3>Innovation Comes in Many Ways</h3></a>
+                                <p style="text-align: justify">At Lamplaimat Pattana School in Buriram, Thailand, teachers use a combination of mindfulness training, teacher learning & feedback sessions, and problem-based learning to improve their students' ability to become smart, mindful, and engaged adults.Last week, Nick worked with a social innovation team to improve the school's ability to scale its solution to more schools in Thailand by using a combination of programmatic, personnel, and tech solutions.Innovation solutions means better schools, better teachers, and better student outcomes.</p>
+                            </div>
+                        </div>
+                        <a href="#" style="text-decoration: none;margin-left: 400px">10 Comments</a>
+                        <hr style="background-color:#0066cc;width: 800px;height: 3px">
+
+                    </center>
+                </div>
+
+            </div>
+        </main>
+
+        <footer>
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-4 foot mt-3">
+                        <h6>Site Map</h6>
+                        <hr>
+                        <ul>
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="ourinspect.php">Our Imapact</a></li>
+                            <li><a href="#">News</a></li>
+                            <li><a href="#about">About Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-4 foot  mt-3">
+
+
+                        <h6>Follow Us </h6>
+                        <hr>
+                        <p>Follow us on social media for all out latest news, updates and event information</p>
+                        <a href="https://www.facebook.com/SchoolProvement"><img src="images/facebook.png" class=""
+                                                                                height="30px" alt=""></a>
+                    </div>
+                    <div class="col-4 foot my-3">
+                        <h6>Contact Us </h6>
+                        <hr>
+                        <form>
+                            <div class="form-group">
+                                <!-- <label>Full Name</label> -->
+                                <input type="text" class="form-control" placeholder="Full Name" required>
+                            </div>
+                            <div class="form-group">
+                                <!-- <label>Full Name</label> -->
+                                <input type="number" class="form-control" placeholder="Phone" required>
+                            </div>
+                            <div class="form-group">
+                                <!-- <label>Full Name</label> -->
+                                <input type="email" class="form-control" placeholder="Email" required>
+                            </div>
+                            <input type="submit" value="Submit" class="btn btn-primary">
+                        </form>
+
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+        </footer>
+
+
+
+        <script src="https://code.jquery.com/jquery-3.4.1.js"
+        integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+                integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+                integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+                integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+        </script>
+        <script src="owlcarousel/dist/owl.carousel.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"
+        integrity="sha256-jDnOKIOq2KNsQZTcBTEnsp76FnfMEttF6AV2DF2fFNE=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js"
+        integrity="sha256-JtQPj/3xub8oapVMaIijPNoM0DHoAtgh/gwFYuN5rik=" crossorigin="anonymous"></script>
+        <script>
+                    $(document).ready(function () {
+                        $(".owl-carousel").owlCarousel();
+
+
+
+                    });
+                    $('.counter').counterUp({
+                        delay: 10,
+                        time: 1000
+                    });
+        </script>
+
+    </body>
+
+</html>
